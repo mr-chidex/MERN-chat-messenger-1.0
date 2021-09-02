3d1b613c3c0687975156a40de6639a7eb21fee6b
mr-chidex <mbahchiboy2@gmail.com>
2021-09-02 14:53:11 +0100

    redux user actions and reducer added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import { userReducer } from "./reducers/userReducers";
+
 const middleware = [thunk];
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  loginUser: userReducer,
+});
 
 const store = createStore(
   reducers,
