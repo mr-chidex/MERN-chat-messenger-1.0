3ecb0d4cdd4b14e4ac89094ce8019830a8ffeb1c
mr-chidex <mbahchiboy2@gmail.com>
2021-09-02 14:52:33 +0100

    redux user actions and reducer added

---
+++ client/src/redux/actions/userActions.js
@@ -0,0 +1,67 @@
+import axios from "../../baseUrl";
+import {
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
+  USER_SIGNUP_FAILED,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAILED,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+export const userSignupAction = (user) => async (dispatch) => {
+  try {
+    const { username, email, password } = user;
+
+    dispatch({ type: USER_SIGNUP_REQUEST });
+
+    await axios({
+      url: "/users/signup",
+      data: { username, email, password },
+      method: "POST",
+    });
+
+    dispatch({ type: USER_SIGNUP_SUCCESS });
+  } catch (error) {
+    dispatch({
+      type: USER_SIGNUP_FAILED,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const userSigninAction =
+  (emailUsername, password) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: USER_SIGNIN_REQUEST });
+
+      const { data } = await axios({
+        url: "/users/signin",
+        data: { emailUsername, password },
+        method: "POST",
+      });
+
+      dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+
+      localStorage.setItem(
+        "CHAT_USER",
+        JSON.stringify(getState().userLogin.user)
+      );
+    } catch (error) {
+      dispatch({
+        type: USER_SIGNIN_FAILED,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const userLogoutAction = () => async (dispatch) => {
+  localStorage.removeItem("CHAT_USER");
+  dispatch({ type: USER_LOGOUT });
+};
---
+++ client/src/redux/reducers/userReducers.js
@@ -0,0 +1,41 @@
+import {
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
+  USER_SIGNUP_FAILED,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAILED,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+
+export const userReducer = (
+  state = {
+    user: localStorage.getItem("CHAT_USER")
+      ? JSON.parse(localStorage.getItem("CHAT_USER"))
+      : null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case USER_SIGNIN_REQUEST:
+      return { loading: true };
+    case USER_SIGNIN_SUCCESS:
+      return { loading: false, user: action.payload };
+    case USER_SIGNIN_FAILED:
+      return { loading: false, error: true, message: action.payload };
+    case USER_SIGNUP_REQUEST:
+      return { loading: true };
+    case USER_SIGNUP_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        message: "signing up successsful",
+      };
+    case USER_SIGNUP_FAILED:
+      return { loading: false, error: true, message: action.payload };
+    case USER_LOGOUT:
+      return { loading: false, user: action.payload };
+    default:
+      return state;
+  }
+};
